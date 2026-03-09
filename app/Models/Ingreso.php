4d1205117ce2b962b53c29924c1fb05d5544745d
Mauro <maurogt.dd@gmail.com>
2026-03-09 10:36:01 -0300

    Fix ingresos y movimientos

--- app/Models/Ingreso.php
+++ app/Models/Ingreso.php
@@ -26,6 +26,8 @@ class Ingreso extends Model
             if ($silo) {
                 $stockAntes = $silo->stock_actual_kg;
                 $silo->increment('stock_actual_kg', $ingreso->cantidad);
+                $silo->update(['estado' => 'activo']);
+                $silo->update(['cereal' => $ingreso->cultivo]);
                 $silo->refresh();
 
                 Log::channel('daily')->info('[Ingreso] Stock incrementado', [
@@ -35,8 +37,37 @@ class Ingreso extends Model
                     'incremento'    => $ingreso->cantidad,
                     'stock_despues' => $silo->stock_actual_kg,
                 ]);
+
             } else {
                 Log::channel('daily')->warning('[Ingreso] Silo no encontrado', [
+                    'silo_destino_buscado' => $ingreso->silo_destino,
+                ]);
+            }
+        });
+
+        static::deleted(function (Ingreso $ingreso) {
+            Log::channel('daily')->info('[Ingreso] Registro eliminado', [
+                'ingreso_id'   => $ingreso->id,
+                'silo_destino' => $ingreso->silo_destino,
+                'cantidad'     => $ingreso->cantidad,
+            ]);
+
+            $silo = Silo::find($ingreso->silo_destino);
+
+            if ($silo) {
+                $stockAntes = $silo->stock_actual_kg;
+                $silo->decrement('stock_actual_kg', $ingreso->cantidad);
+                $silo->refresh();
+
+                Log::channel('daily')->info('[Ingreso] Stock decrementado', [
+                    'silo_id'       => $silo->id,
+                    'silo_nombre'   => $silo->nombre,
+                    'stock_antes'   => $stockAntes,
+                    'decremento'    => $ingreso->cantidad,
+                    'stock_despues' => $silo->stock_actual_kg,
+                ]);
+            } else {
+                Log::channel('daily')->warning('[Ingreso] Silo no encontrado al eliminar', [
                     'silo_destino_buscado' => $ingreso->silo_destino,
                 ]);
             }
--- app/Models/Movimiento.php
+++ app/Models/Movimiento.php
@@ -4,10 +4,70 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Movimiento extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function booted(): void
+    {
+        static::created(function (Movimiento $movimiento) {
+            Log::channel('daily')->info('[Movimiento] Registro creado', [
+                'movimiento_id' => $movimiento->id,
+                'silo_origen'   => $movimiento->silo_origen,
+                'cantidad'      => $movimiento->cantidad,
+            ]);
+
+            $silo = Silo::find($movimiento->silo_origen);
+
+            if ($silo) {
+                $stockAntes = $silo->stock_actual_kg;
+                $silo->decrement('stock_actual_kg', $movimiento->cantidad);
+                $silo->refresh();
+
+                Log::channel('daily')->info('[Movimiento] Stock decrementado', [
+                    'silo_id'       => $silo->id,
+                    'silo_nombre'   => $silo->nombre,
+                    'stock_antes'   => $stockAntes,
+                    'decremento'    => $movimiento->cantidad,
+                    'stock_despues' => $silo->stock_actual_kg,
+                ]);
+            } else {
+                Log::channel('daily')->warning('[Movimiento] Silo no encontrado', [
+                    'silo_origen_buscado' => $movimiento->silo_origen,
+                ]);
+            }
+        });
+
+        static::deleted(function (Movimiento $movimiento) {
+            Log::channel('daily')->info('[Movimiento] Registro eliminado', [
+                'movimiento_id' => $movimiento->id,
+                'silo_origen'   => $movimiento->silo_origen,
+                'cantidad'      => $movimiento->cantidad,
+            ]);
+
+            $silo = Silo::find($movimiento->silo_origen);
+
+            if ($silo) {
+                $stockAntes = $silo->stock_actual_kg;
+                $silo->increment('stock_actual_kg', $movimiento->cantidad);
+                $silo->refresh();
+
+                Log::channel('daily')->info('[Movimiento] Stock restaurado', [
+                    'silo_id'       => $silo->id,
+                    'silo_nombre'   => $silo->nombre,
+                    'stock_antes'   => $stockAntes,
+                    'incremento'    => $movimiento->cantidad,
+                    'stock_despues' => $silo->stock_actual_kg,
+                ]);
+            } else {
+                Log::channel('daily')->warning('[Movimiento] Silo no encontrado al eliminar', [
+                    'silo_origen_buscado' => $movimiento->silo_origen,
+                ]);
+            }
+        });
+    }
 }
